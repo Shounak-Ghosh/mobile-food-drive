5fda26d2fc43a289e983c34572c497aaaecd00a3
BoxofLies86 <134318872+BoxofLies86@users.noreply.github.com>
2025-04-24 22:34:55 -0400

    fixed merge errors

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -34,7 +34,9 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
     db_user = User(
         name=user.name,
         email=user.email,
-        password_hash=hashed_password
+        password_hash=hashed_password,
+        dietary_tags = user.dietaryPreferences
+        
     )
     db.add(db_user)
     db.commit()
